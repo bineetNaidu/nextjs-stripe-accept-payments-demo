119e2b53d9cca38d279af39305bd01c385098f3d
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-19 17:16:04 +0530

    🚀 update component

--- components/FruitCard.tsx
+++ components/FruitCard.tsx
@@ -7,29 +7,50 @@ interface FruitCardProps {
 }
 
 export const FruitCard: FC<FruitCardProps> = ({ fruit }) => {
+  // const handleBuy = () => {
+  //   console.log('Buy clicked');
+  //   fetch('/api/checkout_sessions', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       fruit,
+  //     }),
+  //   });
+  // };
   return (
     <Grid xs={6} sm={3}>
-      <Tooltip content={'Click to buy'} offset={45} color="invert">
-        <Card hoverable clickable>
-          <Card.Body css={{ p: 0 }}>
-            <Card.Image
-              objectFit="cover"
-              src={fruit.img}
-              width="250px"
-              height={140}
-              alt={fruit.title}
-            />
-          </Card.Body>
-          <Card.Footer>
-            <Row wrap="wrap" justify="space-between">
-              <Text b>{fruit.title}</Text>
-              <Text css={{ color: '$accents4', fontWeight: '$semibold' }}>
-                {fruit.price}
-              </Text>
-            </Row>
-          </Card.Footer>
-        </Card>
-      </Tooltip>
+      <form action="/api/checkout_sessions" method="POST">
+        <Tooltip content={'Click to buy'} offset={45} color="secondary">
+          <input
+            type="text"
+            name="fruitName"
+            hidden
+            value={fruit.title}
+            readOnly
+          />
+          <Card hoverable clickable as="button" role="link">
+            <Card.Body css={{ p: 0 }}>
+              <Card.Image
+                objectFit="cover"
+                src={fruit.img}
+                width="250px"
+                height={140}
+                alt={fruit.title}
+              />
+            </Card.Body>
+            <Card.Footer>
+              <Row wrap="wrap" justify="space-between">
+                <Text b>{fruit.title}</Text>
+                <Text css={{ color: '$accents4', fontWeight: '$semibold' }}>
+                  {fruit.price}
+                </Text>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </Tooltip>
+      </form>
     </Grid>
   );
 };
