139fc83e5ca51d21db810372fa184b7328e4ad19
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-19 16:16:46 +0530

    🚀 add NextUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = createTheme({
+    type: 'dark',
+  });
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
