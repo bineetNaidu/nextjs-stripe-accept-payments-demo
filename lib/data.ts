17c93be89c7c2156ea096f6b8033f8605eb700c9
bineetNaidu <bineet.s.naidu@gmail.com>
2022-02-19 16:29:55 +0530

    🐛 extracting data for reusability

---
+++ lib/data.ts
@@ -0,0 +1,42 @@
+export const fruits = [
+  {
+    title: 'Orange',
+    img: 'https://nextui.org/images/fruit-1.jpeg',
+    price: 5.5,
+  },
+  {
+    title: 'Tangerine',
+    img: 'https://nextui.org/images/fruit-2.jpeg',
+    price: 3.0,
+  },
+  {
+    title: 'Raspberry',
+    img: 'https://nextui.org/images/fruit-3.jpeg',
+    price: 10.0,
+  },
+  {
+    title: 'Lemon',
+    img: 'https://nextui.org/images/fruit-4.jpeg',
+    price: 5.3,
+  },
+  {
+    title: 'Advocato',
+    img: 'https://nextui.org/images/fruit-5.jpeg',
+    price: 15.7,
+  },
+  {
+    title: 'Lemon 2',
+    img: 'https://nextui.org/images/fruit-6.jpeg',
+    price: 8.0,
+  },
+  {
+    title: 'Banana',
+    img: 'https://nextui.org/images/fruit-7.jpeg',
+    price: 7.5,
+  },
+  {
+    title: 'Watermelon',
+    img: 'https://nextui.org/images/fruit-8.jpeg',
+    price: 12.2,
+  },
+];
--- pages/api/fruits.ts
+++ pages/api/fruits.ts
@@ -1,48 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { FruitList } from '../../lib/types';
-
-const fruits = [
-  {
-    title: 'Orange',
-    img: 'https://nextui.org/images/fruit-1.jpeg',
-    price: 5.5,
-  },
-  {
-    title: 'Tangerine',
-    img: 'https://nextui.org/images/fruit-2.jpeg',
-    price: 3.0,
-  },
-  {
-    title: 'Raspberry',
-    img: 'https://nextui.org/images/fruit-3.jpeg',
-    price: 10.0,
-  },
-  {
-    title: 'Lemon',
-    img: 'https://nextui.org/images/fruit-4.jpeg',
-    price: 5.3,
-  },
-  {
-    title: 'Advocato',
-    img: 'https://nextui.org/images/fruit-5.jpeg',
-    price: 15.7,
-  },
-  {
-    title: 'Lemon 2',
-    img: 'https://nextui.org/images/fruit-6.jpeg',
-    price: 8.0,
-  },
-  {
-    title: 'Banana',
-    img: 'https://nextui.org/images/fruit-7.jpeg',
-    price: 7.5,
-  },
-  {
-    title: 'Watermelon',
-    img: 'https://nextui.org/images/fruit-8.jpeg',
-    price: 12.2,
-  },
-];
+import { fruits } from '../../lib/data';
 
 export default function handler(
   req: NextApiRequest,
